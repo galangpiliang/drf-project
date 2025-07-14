f4ee8ab29329de567c9f99496b2f260ef0a2c235
Galang <galang.pil@gmail.com>
2025-07-14 13:48:26 +0700

    15. Function Based View Get Method

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
-from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def studentsView(request):
-    students = Student.objects.all()
-    student_list = list(students.values())
-    return JsonResponse(student_list, safe=False)
+    if request.method == 'GET':
+        # Get all the data from the Student table
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
