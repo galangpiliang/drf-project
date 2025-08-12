f4a7a6e489f730b320734d85c990ba07d6508fb3
Galang <galang.pil@gmail.com>
2025-08-12 17:17:24 +0700

    44. Custom Pagination

--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ from django.http import Http404
 from rest_framework import mixins, generics, viewsets
 from blogs.models import Blog, Comment
 from blogs.serializers import BlogSerializer, CommentSerializer
+from .paginations import CustomPagination
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
@@ -152,6 +153,7 @@ def studentDetailView(request, pk):
 class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    pagination_class = CustomPagination
 
 class BlogsView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
