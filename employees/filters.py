564e167f34f4602b8869b754c764cc0eba91e1e5
Galang <galang.pil@gmail.com>
2025-08-13 17:32:19 +0700

    47. Custom Filter Employees By Name And ID

--- employees/filters.py
+++ employees/filters.py
@@ -3,7 +3,9 @@ from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name='designation', lookup_expr='icontains')
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    id = django_filters.RangeFilter(field_name='id')
 
     class Meta:
         model = Employee
-        fields = ['designation']
+        fields = ['designation', 'emp_name', 'id']
