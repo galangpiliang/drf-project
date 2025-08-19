059f27b8bd90cc73569e83f8313fd81fcd948f22
Galang <galang.pil@gmail.com>
2025-08-19 17:22:08 +0700

    49. Search Filters

--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from blogs.models import Blog, Comment
 from blogs.serializers import BlogSerializer, CommentSerializer
 from .paginations import CustomPagination
 from employees.filters import EmployeeFilter
+from rest_framework.filters import SearchFilter
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
@@ -160,6 +161,8 @@ class EmployeeViewset(viewsets.ModelViewSet):
 class BlogsView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['blog_title', 'blog_body']
 
 class CommentsView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
